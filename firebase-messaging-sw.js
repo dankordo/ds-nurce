f5ed2a89c2dafdc1f999cf04566f3006ed9f5011
dankordo <danko1589@gmail.com>
2025-09-02 17:40:50 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,32 @@
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging.js');
+// firebase-messaging-sw.js
 
+// Используем compat-версии для Service Worker
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
+
+// Инициализация Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyBUUrP-CevoeDOsUZorjC6XDзZ-vd3Uauw",
+    apiKey: "BEp5FjaMusKOcJPBREZEad0ZWHMMyYAp0dl_6yeFaiNdqfrtWFJ_0lA0S5kU8usP_IJu0SkdsTrSkOsRzPGYKY4",
     authDomain: "ds-nurce-bdc37.firebaseapp.com",
-    databaseURL: "https://ds-nurce-bdc37-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "ds-nurce-bdc37",
     storageBucket: "ds-nurce-bdc37.appspot.com",
     messagingSenderId: "725307523922",
     appId: "1:725307523922:web:9bb5c6ae685e479719b08b"
 });
 
+// Получаем объект messaging
 const messaging = firebase.messaging();
 
+// Обработка фоновых сообщений
 messaging.onBackgroundMessage(function(payload) {
-    const notificationTitle = payload.notification?.title || 'MidwifeApp';
-    const notificationOptions = {
-        body: payload.notification?.body || '',
-        icon: '/favicon.png'
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    console.log('[firebase-messaging-sw.js] Received background message', payload);
+
+    if (payload.notification) {
+        const notificationTitle = payload.notification.title;
+        const notificationOptions = {
+            body: payload.notification.body,
+            icon: '/favicon.png'
+        };
+        self.registration.showNotification(notificationTitle, notificationOptions);
+    }
 });
